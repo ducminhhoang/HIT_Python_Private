5228fd12309cf050433fb5ed1420f476f6591c54
= <=>
2022-11-23 15:07:54 +0700

    Buoi4

---
+++ Buoi4/test1.py
@@ -0,0 +1,30 @@
+# def hieu(a, b, c):
+#     return a-b-c
+#
+# print(hieu(5, b=2, c=5))
+
+# doi so chua đc khai bao, mac dinh, *a, **b
+# def hhh(a, b, c, *name):
+#     return sum(name)
+# print(hhh(1, 2,"minh",1))
+
+# def tinhToan(a,b):
+#     """
+#
+#     :param b: Phan tu thu nhat
+# :param a: Phan thu hai
+# :return : a+b, a-b, a/b, a*b
+#     """
+#     return a+b, a-b, a/b, a*b
+# print(tinhToan(6,3))
+#
+# help (tinhToan)
+
+
+# bien dinh kem
+# nonlocal x
+
+
+
+# class car:
+#     def __init__(self):
